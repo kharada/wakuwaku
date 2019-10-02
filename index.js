d19a3090512c52f32785437130abcf0fbdb126b0
Keiji Harada <keikei_moutoku@yahoo.co.jp>
2019-10-03 01:08:02 +0900

    October script.

---
+++ index.js
@@ -0,0 +1,108 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    // If headless is false, you can see what is going on your browser. 
+    const browser = await puppeteer.launch({headless:true});
+    const page = await browser.newPage();
+    page.setViewport({width:1200, height:800});
+    let xpath;
+
+    // Parameters.
+    const email = "your email";
+    const passwd = "your password";
+    const dateYetInputClass = 'jwSCmU';
+    const uncheckInputSelector = 'label.fYYSDF';
+    const closeButtonSelector = 'button.MdANq';
+
+    // Date and Dummy Steps.
+    let targetDate = new Date().getDate();
+    let steps = 8000 + Math.floor(1000 * Math.random());
+
+    try {
+
+        // Open pepup.
+        await page.goto('https://pepup.life/users/sign_in');
+  
+        // Here is Login page.
+        await page.type('#sender-email', email);
+        await page.type('#user-pass', passwd);
+        await page.click('input[type="submit"]');
+    
+        // Here is Top page.
+        xpath = `//div[contains(text(), '健康わくわくマイレージの入力はこちらから')]`;
+        await page.waitForXPath(xpath);
+        await (await page.$x(xpath))[0].click();
+    
+        // Go to the next page.
+        xpath = `//a[contains(text(), '▶2019年度わくわくマイレージ')]`;
+        await page.waitForXPath(xpath);
+        await (await page.$x(xpath))[0].click();
+        
+        // Here is daily input page.
+    
+        // Life style (Other Food)
+        xpath = `//button[contains(., '` + targetDate + `') and contains(@class,'` + dateYetInputClass + `')]`;
+        await page.waitForXPath(xpath);
+        await (await page.$x(xpath))[4].click();
+        await page.waitForSelector(uncheckInputSelector);
+        const labels = await page.$$(uncheckInputSelector);
+        for(const label of labels){
+            await label.click();
+        }
+        await page.click(closeButtonSelector);
+    
+        // Life style (Drink Alchole?)
+        xpath = `//button[contains(., '` + targetDate + `') and contains(@class,'` + dateYetInputClass + `')]`;
+        await page.waitForXPath(xpath);
+        await (await page.$x(xpath))[3].click();
+        await page.waitForSelector(uncheckInputSelector);
+        await page.click(uncheckInputSelector);
+        await page.click(closeButtonSelector);
+    
+        // Sleeping custom.
+        xpath = `//button[contains(., '` + targetDate + `') and contains(@class,'` + dateYetInputClass + `')]`;
+        await page.waitForXPath(xpath);
+        await (await page.$x(xpath))[2].click();
+        await page.waitForSelector(uncheckInputSelector);
+        await page.click(uncheckInputSelector);
+        await page.click(closeButtonSelector);
+    
+        // Sleeping time.
+        xpath = `//button[contains(., '` + targetDate + `') and contains(@class,'` + dateYetInputClass + `')]`;
+        await page.waitForXPath(xpath);
+        await (await page.$x(xpath))[1].click();
+        await page.waitForSelector('input[name="vitalInput"]');
+        await page.click('input[name="vitalInput"]');
+        await page.type('input[name="vitalInput"]', '7');
+        await page.click('button[type="submit"]');
+        await page.waitFor(3000);
+    
+        // Step count.
+        xpath = `//button[contains(., '` + targetDate + `') and contains(@class,'` + dateYetInputClass + `')]`;
+        await page.waitForXPath(xpath);
+        await (await page.$x(xpath))[0].click();
+        await page.waitForSelector('input[name="vitalInput"]');
+        await page.click('input[name="vitalInput"]');
+        await page.type('input[name="vitalInput"]', steps + '');
+        await page.click('button[type="submit"]');
+        await page.waitFor(3000);
+    
+        // Click the other checkboxes.
+        xpath = `//span[contains(text(), '日々の記録')]`;
+        await page.waitForXPath(xpath);
+        await (await page.$x(xpath))[0].click();
+        xpath = `//input[not (@checked) and @type = 'checkbox']/..`;
+        await page.waitForXPath(xpath);
+        const otherLabels =  await page.$x(xpath);
+        for(const item of otherLabels){
+            await item.click();
+        }
+     
+    } catch (error) {
+        console.error(error);
+    } finally {
+        // Finish and Close the browser.
+        await browser.close();
+    }
+  })();
+
