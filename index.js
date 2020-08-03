e06daf92f26f49903f9a908661c82693a655a981
Keiji Harada <keikei_moutoku@yahoo.co.jp>
2020-08-03 09:33:20 +0900

    mod for august

--- index.js
+++ index.js
@@ -43,6 +43,14 @@ const puppeteer = require('puppeteer');
 
         // Here is daily input page.
 
+        // Life style (Plus 10, August)
+        xpath = `//button[contains(., '` + targetDate + `') and contains(@class,'` + dateYetInputClass + `')]`;
+        await page.waitForXPath(xpath);
+        await (await page.$x(xpath))[5].click();
+        await page.waitForSelector(uncheckInputSelector);
+        await page.click(uncheckInputSelector);
+        await page.click(closeButtonSelector);
+        
         // Life style (Other Food)
         xpath = `//button[contains(., '` + targetDate + `') and contains(@class,'` + dateYetInputClass + `')]`;
         await page.waitForXPath(xpath);
