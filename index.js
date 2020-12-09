4a9b807cf368fdb2ccdb46c051b665ea95674674
Keiji Harada <keikei_moutoku@yahoo.co.jp>
2020-12-09 17:11:27 +0900

    updete for dec.

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const puppeteer = require('puppeteer');
     // Parameters.
     const email = params.username;
     const passwd = params.password;
-    const dateYetInputClass = 'bpilVq';
-    const uncheckInputSelector = 'label.jnIMse';
-    const closeButtonSelector = 'button.leagUd';
+    const dateYetInputClass = 'jZAaAL';
+    const uncheckInputSelector = 'label.kmktFr';
+    const closeButtonSelector = 'button.eYNZaQ';
 
     // Date and Dummy Steps.
     let targetDate = new Date().getDate();
